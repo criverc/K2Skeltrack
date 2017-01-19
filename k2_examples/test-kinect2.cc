e3758266a6868b3438526d6617ec9527985b86ed
Ben Blumer <ben.blumer@istuary.com>
2017-01-18 16:56:45 -0800

    Moved examples for kinect2 into their own folder. Now to use Kinect2 (and its examples), there's no need to install gfreenect

--- k2_examples/test-kinect2.cc
+++ k2_examples/test-kinect2.cc
@@ -1,14 +1,20 @@
-#include <gfreenect.h>
+#include <iostream>
+#include <string.h>
+#include <signal.h>
+
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/registration.h>
+#include <libfreenect2/packet_pipeline.h>
+
 #include <skeltrack.h>
 #include <math.h>
-#include <string.h>
 #include <cairo.h>
 #include <glib-object.h>
 #include <clutter/clutter.h>
 #include <clutter/clutter-keysyms.h>
 
 static SkeltrackSkeleton *skeleton = NULL;
-static GFreenectDevice *kinect = NULL;
 static ClutterActor *info_text;
 static ClutterActor *skel_tex;
 static ClutterActor *depth_tex;
@@ -17,14 +23,25 @@ static ClutterContent *depth_canvas;
 static ClutterContent *depth_image = NULL;
 static SkeltrackJointList list = NULL;
 
-static enum { SKELETON, CLOUD, BOTH } SHOW = BOTH;
+static gboolean SHOW_SKELETON = TRUE;
 static gboolean ENABLE_SMOOTHING = TRUE;
-static gfloat SMOOTHING_FACTOR = .0;
+static gfloat SMOOTHING_FACTOR = .5;
 
 static guint THRESHOLD_BEGIN = 500;
 /* Adjust this value to increase of decrease
    the threshold */
-static guint THRESHOLD_END   = 1500;
+static guint THRESHOLD_END   = 3000;
+static gint DIMENSION_REDUCTION = 8;
+
+void* kinect = NULL;
+
+libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
+libfreenect2::FrameMap frames;
+libfreenect2::Registration* registration = NULL;
+libfreenect2::Frame undistorted(512, 424, 4);
+libfreenect2::Frame registered(512, 424, 4);
+size_t framecount = 0;
+
 
 typedef struct
 {
@@ -101,8 +118,7 @@ process_buffer (guint16 *buffer,
   reduced_width = (width - width % dimension_factor) / dimension_factor;
   reduced_height = (height - height % dimension_factor) / dimension_factor;
 
-  reduced_buffer = g_slice_alloc0 (reduced_width * reduced_height *
-                                   sizeof (guint16));
+  reduced_buffer = (guint16*) g_slice_alloc0 (reduced_width * reduced_height * sizeof (guint16));
 
   for (i = 0; i < reduced_width; i++)
     {
@@ -145,7 +161,7 @@ create_grayscale_buffer (BufferInfo *buffer_info, gint dimension_reduction)
   reduced_buffer = buffer_info->reduced_buffer;
 
   size = buffer_info->width * buffer_info->height * sizeof (guchar) * 3;
-  grayscale_buffer = g_slice_alloc (size);
+  grayscale_buffer = (guchar*) g_slice_alloc (size);
   /*Paint is white*/
   memset (grayscale_buffer, 255, size);
 
@@ -153,41 +169,53 @@ create_grayscale_buffer (BufferInfo *buffer_info, gint dimension_reduction)
     {
       for (j = 0; j < buffer_info->reduced_height; j++)
         {
+ 
           if (reduced_buffer[j * buffer_info->reduced_width + i] != 0)
             {
+//              grayscale_buffer_set_value (grayscale_buffer, index, 0);
               gint index = j * dimension_reduction * buffer_info->width +
                 i * dimension_reduction;
-              grayscale_buffer_set_value (grayscale_buffer, index, 0);
+
+              grayscale_buffer[index * 3] = 255;
+              grayscale_buffer[index * 3 + 1] = 0;
+              grayscale_buffer[index * 3 + 2] = 0;
+
             }
-        }
+         }
     }
 
   return grayscale_buffer;
 }
 
 static void
-on_depth_frame (GFreenectDevice *kinect, gpointer user_data)
+on_depth_frame (libfreenect2::Frame* frame)
 {
   gboolean smoothing_enabled;
   gint width, height;
-  gint dimension_factor;
+  gint dimension_factor = DIMENSION_REDUCTION;
   guchar *grayscale_buffer;
   guint16 *depth;
   BufferInfo *buffer_info;
   gsize len;
   GError *error = NULL;
-  GFreenectFrameMode frame_mode;
   ClutterContent *content;
 
-  depth = (guint16 *) gfreenect_device_get_depth_frame_raw (kinect,
-                                                            &len,
-                                                            &frame_mode);
+  width = frame->width;
+  height = frame->height;
 
-  width = frame_mode.width;
-  height = frame_mode.height;
+  depth = (guint16*) g_slice_alloc (width * height * sizeof (guint16));
 
-  g_object_get (skeleton, "dimension-reduction", &dimension_factor, NULL);
+  /* convert buffer from float* to guint16* so it can be processed by skeltrack */
+  for (int i=0; i < height; i++) 
+  {
+    for (int j=0; j < width; j++)
+    {
+        depth[i*width + j] = (guint16) (((float*) (frame->data))[i*width+j]);
+    }
+  } 
 
+  g_object_set (skeleton, "dimension-reduction", dimension_factor, NULL);
+  
   buffer_info = process_buffer (depth,
                                 width,
                                 height,
@@ -205,9 +233,7 @@ on_depth_frame (GFreenectDevice *kinect, gpointer user_data)
 
 
   content = clutter_actor_get_content (depth_tex);
-
-  grayscale_buffer = create_grayscale_buffer (buffer_info,
-                                              dimension_factor);
+  grayscale_buffer = create_grayscale_buffer (buffer_info, dimension_factor);
 
   if (depth_image == NULL)
     depth_image = clutter_image_new ();
@@ -216,38 +242,38 @@ on_depth_frame (GFreenectDevice *kinect, gpointer user_data)
   if (! clutter_image_set_data (CLUTTER_IMAGE (depth_image),
                                 grayscale_buffer,
                                 COGL_PIXEL_FORMAT_RGB_888,
-                                width, height,
-                                0,
-                                &error))
-  {
-    g_debug ("Error setting texture area: %s", error->message);
-    g_error_free (error);
-  }
-
-  g_slice_free1 (width * height * sizeof (guchar) * 3, grayscale_buffer);
-}
-
-static void
-on_video_frame (GFreenectDevice *kinect, gpointer user_data)
-{
-  guchar *buffer;
-  GError *error = NULL;
-  GFreenectFrameMode frame_mode;
-  ClutterContent *content;
-
-  buffer = gfreenect_device_get_video_frame_rgb (kinect, NULL, &frame_mode);
-  content = clutter_actor_get_content (video_tex);
-
-  if (! clutter_image_set_data (CLUTTER_IMAGE (content),
-                                buffer,
-                                COGL_PIXEL_FORMAT_RGB_888,
-                                frame_mode.width, frame_mode.height,
+                                width,
+                                height,
                                 0,
                                 &error))
     {
       g_debug ("Error setting texture area: %s", error->message);
       g_error_free (error);
     }
+
+  g_slice_free1 (width * height * sizeof (guchar) * 3, grayscale_buffer);
+  g_slice_free1 (width * height * sizeof (guint16), depth);
+}
+
+static void
+on_video_frame (libfreenect2::Frame* frame)
+{
+  GError *error = NULL;
+  ClutterContent *content;
+
+  content = clutter_actor_get_content (video_tex);
+
+  if (! clutter_image_set_data (CLUTTER_IMAGE (content),
+                                frame->data,
+                                COGL_PIXEL_FORMAT_BGRA_8888,
+                                frame->width, frame->height,
+                                0,
+                                &error))
+    {
+      g_debug ("Error setting texture area: %s", error->message);
+      g_error_free (error);
+    }
+
 }
 
 static void
@@ -342,17 +368,25 @@ on_skeleton_draw (ClutterCanvas *canvas,
 
   connect_joints (cairo, left_shoulder, right_shoulder, "#afafaf");
 
-  connect_joints (cairo, left_shoulder, left_elbow, "#afafaf");
+  connect_joints (cairo, left_shoulder, left_hand, "#afafaf");
 
-  connect_joints (cairo, right_shoulder, right_elbow, "#afafaf");
+  connect_joints (cairo, right_shoulder, right_hand, "#afafaf");
 
-  connect_joints (cairo, right_hand, right_elbow, "#afafaf");
+//  connect_joints (cairo, right_hand, right_elbow, "#afafaf");
 
-  connect_joints (cairo, left_hand, left_elbow, "#afafaf");
+//  connect_joints (cairo, left_hand, left_elbow, "#afafaf");
 
   paint_joint (cairo, left_hand, 30000, "#C2FF00");
 
+  paint_joint (cairo, left_shoulder, 20000, "#C2FF00");
+
+//  paint_joint (cairo, left_elbow, 20, "#C2FF00");
+
   paint_joint (cairo, right_hand, 30000, "#00FAFF");
+
+  paint_joint (cairo, right_shoulder, 20000, "#00FAFF");
+
+//  paint_joint (cairo, right_elbow, 20, "#00FAFF");
 
   skeltrack_joint_list_free (list);
   list = NULL;
@@ -364,11 +398,11 @@ static void
 set_info_text (void)
 {
   gchar *title;
-  title = g_strdup_printf ("<b>Current View:</b> %s\t"
+  title = g_strdup_printf ("<b>Current View:</b> %s\t\t\t"
                            "<b>Threshold:</b> %d\n"
                            "<b>Smoothing Enabled:</b> %s\t\t\t"
                            "<b>Smoothing Level:</b> %.2f",
-                           SHOW == SKELETON ? "Skeleton\t\t" : SHOW == CLOUD ? "Point Cloud\t\t" : "Skeleton and Point Cloud",
+                           SHOW_SKELETON ? "Skeleton" : "Point Cloud",
                            THRESHOLD_END,
                            ENABLE_SMOOTHING ? "Yes" : "No",
                            SMOOTHING_FACTOR);
@@ -386,23 +420,10 @@ set_threshold (gint difference)
 }
 
 static void
-set_tilt_angle (GFreenectDevice *kinect, gdouble difference)
+set_tilt_angle (void *kinect, gdouble difference)
 {
-  GError *error = NULL;
-  gdouble angle;
-  angle = gfreenect_device_get_tilt_angle_sync (kinect, NULL, &error);
-  if (error != NULL)
-    {
-      g_error_free (error);
-      return;
-    }
-
-  if (angle >= -31 && angle <= 31)
-    gfreenect_device_set_tilt_angle (kinect,
-                                     angle + difference,
-                                     NULL,
-                                     NULL,
-                                     NULL);
+    printf ("nothing to do here, kinect v2 has no motor ...\n");
+    fflush (0);
 }
 
 static void
@@ -428,27 +449,24 @@ on_key_release (ClutterActor *actor,
                 ClutterEvent *event,
                 gpointer data)
 {
-  GFreenectDevice *kinect;
+  void *kinect;
   gdouble angle;
   guint key;
   g_return_val_if_fail (event != NULL, FALSE);
-
-  kinect = GFREENECT_DEVICE (data);
 
   key = clutter_event_get_key_symbol (event);
   switch (key)
     {
     case CLUTTER_KEY_space:
-      SHOW= (SHOW + 1) % 3;
-      if (SHOW == SKELETON)   clutter_actor_set_opacity (skel_tex, 255);
-      else if (SHOW == CLOUD) clutter_actor_set_opacity (skel_tex, 0);
-      else                    clutter_actor_set_opacity (skel_tex, 122);
+      SHOW_SKELETON = !SHOW_SKELETON;
+      if (SHOW_SKELETON) clutter_actor_set_opacity (skel_tex, 122);
+      else               clutter_actor_set_opacity (skel_tex, 0);
       break;
     case CLUTTER_KEY_plus:
-      set_threshold (100);
+      set_threshold (10);
       break;
     case CLUTTER_KEY_minus:
-      set_threshold (-100);
+      set_threshold (-10);
       break;
     case CLUTTER_KEY_Up:
       set_tilt_angle (kinect, 5);
@@ -480,7 +498,7 @@ create_instructions (void)
   clutter_text_set_markup (CLUTTER_TEXT (text),
                          "<b>Instructions:</b>\n"
                          "\tChange between skeleton\n"
-                         "\t  tracking, threshold view or both:  \tSpace bar\n"
+                         "\t  tracking and threshold view:  \tSpace bar\n"
                          "\tSet tilt angle:  \t\t\t\tUp/Down Arrows\n"
                          "\tIncrease threshold:  \t\t\t+/-\n"
                          "\tEnable/Disable smoothing:  \t\ts\n"
@@ -492,49 +510,59 @@ create_instructions (void)
 static void
 on_destroy (ClutterActor *actor, gpointer data)
 {
-  ClutterContent *content;
-  GFreenectDevice *device = GFREENECT_DEVICE (data);
-
-  gfreenect_device_stop_depth_stream (device, NULL);
-  gfreenect_device_stop_video_stream (device, NULL);
   clutter_main_quit ();
 }
 
 static void
-on_new_kinect_device (GObject      *obj,
-                      GAsyncResult *res,
-                      gpointer      user_data)
+quit (gint signale)
+{
+  signal (SIGINT, 0);
+
+  clutter_main_quit ();
+}
+
+
+void on_timeline_new_frame (ClutterTimeline *timeline, gint frame_num, gpointer data) 
+{
+    listener.waitForNewFrame (frames);
+    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+
+    registration->apply (rgb, depth, &undistorted, &registered);
+
+    framecount++;
+    if (framecount % 100 == 0)
+        std::cout << "Received " << framecount << " frames." << std::endl;
+
+    on_video_frame (rgb);
+    on_depth_frame (depth);
+
+    listener.release (frames);
+}
+
+int
+main (int argc, char *argv[])
 {
   ClutterActor *stage, *instructions;
   GError *error = NULL;
-  gint width = 640;
-  gint height = 480;
+  gint width = 512;
+  gint height = 424;
 
-  kinect = gfreenect_device_new_finish (res, &error);
-  if (kinect == NULL)
-    {
-      g_debug ("Failed to created kinect device: %s", error->message);
-      g_error_free (error);
-      clutter_main_quit ();
-      return;
-    }
-
-  g_debug ("Kinect device created!");
+  if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
+    return -1;
 
   stage = clutter_stage_new ();
-  clutter_stage_set_title (CLUTTER_STAGE (stage), "Kinect Test");
+  clutter_stage_set_title (CLUTTER_STAGE (stage), "Kinect v2 Test");
   clutter_actor_set_size (stage, width * 2, height + 250);
   clutter_stage_set_user_resizable (CLUTTER_STAGE (stage), TRUE);
 
-  g_signal_connect (stage, "destroy", G_CALLBACK (on_destroy), kinect);
-  g_signal_connect (stage,
-                    "key-release-event",
-                    G_CALLBACK (on_key_release),
-                    kinect);
+  g_signal_connect (stage, "destroy", G_CALLBACK (on_destroy), NULL);
+  g_signal_connect (stage, "key-release-event", G_CALLBACK (on_key_release), NULL);
 
   depth_tex = clutter_actor_new ();
   clutter_actor_set_size (depth_tex, width, height);
-  clutter_actor_add_child (stage, depth_tex);
+  clutter_actor_add_child (stage, depth_tex); 
   clutter_actor_set_position (depth_tex, 0.0, 0.0);
 
   skel_tex = clutter_actor_new ();
@@ -554,7 +582,7 @@ on_new_kinect_device (GObject      *obj,
 
   info_text = clutter_text_new ();
   clutter_actor_set_position (info_text, 50, height + 20);
-  clutter_actor_add_child (stage, info_text);
+  clutter_actor_add_child (stage, info_text); 
 
   instructions = create_instructions ();
   clutter_actor_set_position (instructions, 50, height + 70);
@@ -563,59 +591,110 @@ on_new_kinect_device (GObject      *obj,
   skeleton = skeltrack_skeleton_new ();
   g_object_get (skeleton, "smoothing-factor", &SMOOTHING_FACTOR, NULL);
 
+  g_object_set (skeleton, "hands-minimum-distance", 300,
+                          "shoulder-circumference-radius", 200,
+                          NULL);
+
   set_info_text ();
-
-  g_signal_connect (kinect,
-                    "depth-frame",
-                    G_CALLBACK (on_depth_frame),
-                    NULL);
-
-  g_signal_connect (kinect,
-                    "video-frame",
-                    G_CALLBACK (on_video_frame),
-                    NULL);
 
   g_signal_connect (depth_canvas,
                     "draw",
                     G_CALLBACK (on_skeleton_draw),
                     NULL);
 
-  gfreenect_device_set_tilt_angle (kinect, 0, NULL, NULL, NULL);
-
-  gfreenect_device_start_depth_stream (kinect,
-                                       GFREENECT_DEPTH_FORMAT_MM,
-                                       NULL);
-
-  gfreenect_device_start_video_stream (kinect,
-                                       GFREENECT_RESOLUTION_MEDIUM,
-                                       GFREENECT_VIDEO_FORMAT_RGB, NULL);
-
   clutter_actor_show (stage);
-}
-
-static void
-quit (gint signale)
-{
-  signal (SIGINT, 0);
-
-  clutter_main_quit ();
-}
-
-int
-main (int argc, char *argv[])
-{
-  if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
-    return -1;
-
-  gfreenect_device_new (0,
-                        GFREENECT_SUBDEVICE_CAMERA,
-                        NULL,
-                        on_new_kinect_device,
-                        NULL);
 
   signal (SIGINT, quit);
 
+  ClutterTimeline *timeline = clutter_timeline_new (1);
+  g_signal_connect (timeline, "new-frame", G_CALLBACK (on_timeline_new_frame), NULL);
+  clutter_timeline_set_repeat_count (timeline, -1);
+  clutter_timeline_start (timeline);
+
+  libfreenect2::Freenect2Device *dev = 0;
+  libfreenect2::PacketPipeline *pipeline = 0;
+
+  libfreenect2::Freenect2 freenect2;
+
+  if(freenect2.enumerateDevices() == 0)
+  {
+    std::cout << "no device connected!" << std::endl;
+    return -1;
+  }
+
+  std::string serial = freenect2.getDefaultDeviceSerialNumber();
+
+  for(int argI = 1; argI < argc; ++argI)
+  {
+    const std::string arg(argv[argI]);
+
+    if(arg == "cpu")
+    {
+      if(!pipeline)
+        pipeline = new libfreenect2::CpuPacketPipeline();
+    }
+    else if(arg == "gl")
+    {
+#ifdef LIBFREENECT2_WITH_OPENGL_SUPPORT
+      if(!pipeline)
+        pipeline = new libfreenect2::OpenGLPacketPipeline();
+#else
+      std::cout << "OpenGL pipeline is not supported!" << std::endl;
+#endif
+    }
+    else if(arg == "cl")
+    {
+#ifdef LIBFREENECT2_WITH_OPENCL_SUPPORT
+      if(!pipeline)
+        pipeline = new libfreenect2::OpenCLPacketPipeline();
+#else
+      std::cout << "OpenCL pipeline is not supported!" << std::endl;
+#endif
+    }
+    else if(arg.find_first_not_of("0123456789") == std::string::npos) //check if parameter could be a serial number
+    {
+      serial = arg;
+    }
+    else
+    {
+      std::cout << "Unknown argument: " << arg << std::endl;
+    }
+  }
+
+  if(pipeline)
+  {
+    dev = freenect2.openDevice(serial, pipeline);
+  }
+  else
+  {
+    dev = freenect2.openDevice(serial);
+  }
+
+  if(dev == 0)
+  {
+    std::cout << "failure opening device!" << std::endl;
+    return -1;
+  }
+
+  dev->setColorFrameListener(&listener);
+  dev->setIrAndDepthFrameListener(&listener);
+  dev->start();
+
+  std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+  std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+
+  registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+
+  framecount = 0;
+
   clutter_main ();
+
+  dev->stop();
+  dev->close();
+
+  delete registration;
+
+  g_object_unref (timeline);
 
   if (kinect != NULL)
     g_object_unref (kinect);
